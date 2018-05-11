fe3d7569b55eba9ba2597d51123970a5d3320c08
Matthew Plein <mjplein@gmail.com>
2018-05-11 12:50:02 -0500

    pushing login page for merge to master

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
+import Login from './components/Pages/Login';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-    
-     <p>WORKING</p>
+     <div> 
+      <p>WORKING</p>
+      <Login/>
+     </div>
     );
   }
 }
