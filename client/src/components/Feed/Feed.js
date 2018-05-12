083f99309f90611e3f0eb1a646c04a420956ff45
Don Moldenhauer <dmmoldenhauer@gmail.com>
2018-05-12 17:56:35 -0500

    roughed in card for feed

---
+++ client/src/components/Feed/Feed.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Feed.css";
+
+
+const Feed = (props) => (
+    <div className="container">
+
+        <div class="card">
+            <div class="card-body">
+               card contents go here
+            </div>
+        </div>
+
+
+
+
+    </div>
+);
+
+export default Feed;
