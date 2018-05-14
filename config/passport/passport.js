445d54c154feff4c9bf29942596ff4201901a499
Mark Mankey <mark.mankey@gmail.com>
2018-05-14 13:31:58 -0500

    sign in functional

--- config/passport/passport.js
+++ config/passport/passport.js
@@ -4,34 +4,36 @@ var bCrypt = require('bcrypt-nodejs');
 console.log("Passport.js is loaded")
 
 module.exports = function (passport, user) {
-  console.log("Setting up Authentication")
+  console.log("----Setting up Authentication----")
   var User = user;
   var LocalStrategy = require('passport-local').Strategy;
 
 
-
+  //LOCAL SIGN-UP
   passport.use('local-signup', new LocalStrategy(
 
     {
-      usernameField: 'email',
+      usernameField: 'email',  // this property called email but had to be changed to usernameField to use the email as a login
       passwordField: 'password',
       passReqToCallback: true // allows us to pass back the entire request to the callback
     },
 
     function (req, email, password, done) {
-      console.log("passport.js inside signUp function!")
+      console.log("passport.js, INSIDE SIGNUP FUNCTION!")
 
       var generateHash = function (password) {
         return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
       };
 
       User.findOne({ where: { email: email } }).then(function (user) {
-        console.log("findone in passport function")
+        // console.log("findone in passport function")
         if (user) {
+          console.log("passport.js, Hey there is ALREADY ANOTHER USER WITH THAT EMAIL!")
           return done(null, false, { message: 'That email is already taken' });
         }
 
         else {
+          console.log("passport.js, sign-up, NEW USER CREATED!")
           var userPassword = generateHash(password);
           var data =
             {
@@ -59,19 +61,19 @@ module.exports = function (passport, user) {
     }
   ));
 
-  //LOCAL SIGNIN
+  //LOCAL SIGN-IN
   passport.use('local-signin', new LocalStrategy(
 
     {
 
       // by default, local strategy uses username and password, we will override with email
-      emailField: 'email',
+      usernameField: 'email', // changed from emailField to usernameField... had to do it for signup... probabbly need to here too
       passwordField: 'password',
       passReqToCallback: true // allows us to pass back the entire request to the callback
     },
 
     function (req, email, password, done) {
-      console.log("inside sign in")
+      console.log("passport.js, inside LOCAL SIGN IN")
       var User = user;
 
       var isValidPassword = function (userpass, password) {
@@ -89,7 +91,8 @@ module.exports = function (passport, user) {
           return done(null, false, { message: 'Incorrect password.' });
 
         }
-
+        //Valid user functions below... I think
+        console.log("passport.js, SIGN-IN VALID USER!!!")
         var userinfo = user.get();
 
         return done(null, userinfo);
