8032f4c3fcfdc09c6a594e88c1e14ad53575ef35
Mark Mankey <mark.mankey@gmail.com>
2018-05-15 10:35:03 -0500

    update islogin status

--- routes/isLogIn.js
+++ routes/isLogIn.js
@@ -10,7 +10,7 @@ function isLoggedIn(req, res, next) {
     if (req.isAuthenticated())
         return next();
 
-    res.redirect('/signin');
+    res.status(403).send(err);
 }
 
 module.exports = isLoggedIn;
